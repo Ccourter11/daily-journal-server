932415b47f9c81f35793b5d5acbfe439964864d9
Ccourter11 <courtercalvin@yahoo.com>
2021-04-23 12:00:31 -0500

    trying to fix a bug

--- dailyjournal.sql
+++ dailyjournal.sql
@@ -19,7 +19,8 @@ CREATE TABLE `Moods` (
 
 SELECT * FROM Entries
 
-INSERT INTO `Entries` VALUES (null, "2021-04-15", "SQL", "databases are amazing", 1);
+INSERT INTO `Entries` VALUES (null, "2021-04-15", "SQL", "databases are deep", 1);
+INSERT INTO `Entries` VALUES (null, "2021-04-22", "SQLite", "I have much to learn", 9);
 INSERT INTO `Entries` VALUES (null, "2021-04-20", "SQLite", "oh boy", 3);
 
 INSERT INTO `Moods` VALUES (null, "Happy");
@@ -30,10 +31,16 @@ INSERT INTO `Moods` VALUES (null, "Stressed");
 INSERT INTO `Moods` VALUES (null, "Sad");
 INSERT INTO `Moods` VALUES (null, "Anxious");
 INSERT INTO `Moods` VALUES (null, "Angry");
+INSERT INTO `Moods` VALUES (null, "Eager");
 
 
-
-
-
-
-
+SELECT
+	e.id,
+	e.date,
+	e.text,
+	e.concept,
+	e.moodId,
+	m.mood mood
+FROM Entries e	
+LEFT JOIN Moods m
+	ON m.id = e.moodId
